d4b83079f85262c302ba5c03947cc9e00cfe7442
Oliver B. <oliver.boettner.hst@gmail.com>
2024-12-01 06:50:50 +0100

    day 1 second thoughts

--- 2024/day 1/solultionV2.py
+++ 2024/day 1/solultionV2.py
@@ -1,5 +1,5 @@
 # Open a file in read mode
-with open('input.txt', 'r') as file:
+with open('C:\\Users\\olive\\OneDrive\\Informatik\\Advent of Code\\2024\\day 1\\input.txt', 'r') as file:
     content = file.read()
 
 # Print the content of the file
@@ -9,8 +9,8 @@ l2 =[]
 
 for line in content.split('\n'):    
     words = line.split()
-    l1.append(words[0])
-    l2.append(words[1])
+    l1.append(int(words[0]))
+    l2.append(int(words[1]))
 
 l1.sort()
 l2.sort()
@@ -18,14 +18,14 @@ l2.sort()
 sum = 0
 
 for i in range(len(l1)):
-    sum += abs(int(l1[i]) - int(l2[i]))
-    
+    sum += abs(l1[i] - l2[i])
+
 print(sum)
 
 sum = 0
 
 for i in l1:
-    sum += l2.count(i)*int(i)
+    sum += l2.count(i)*i
 
 print(sum)
     
